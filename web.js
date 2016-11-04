75b01abb11e542141a8ac8681e476d5365c33460
pavel.sverdlov <pavel.sverdlov@techinsign.ua>
2016-11-04 14:08:37 +0200

    OPENSHIFT_NODEJS

--- web.js
+++ web.js
@@ -9,7 +9,8 @@ var app = express();
 //var db = repository.getConnection();
 
 app.configure(function(){
-  app.set('port', process.env.PORT || 3000);
+  app.set('port', process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 3000);
+  app.set('ip', process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1");
   app.set('views', __dirname + '/views');
   app.set('view engine', 'ejs');
   app.use(express.favicon());
