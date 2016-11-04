203f240c01d354f21e9b4dbac935c37c18dbb9be
pavel.sverdlov <pavel.sverdlov@techinsign.ua>
2016-11-04 14:13:18 +0200

    OPENSHIFT_NODEJS

--- web.js
+++ web.js
@@ -35,6 +35,6 @@ app.configure('development', function(){
 
 routes.init(app);
 
-http.createServer(app).listen(app.get('port'), function(){
+http.createServer(app).listen(app.get('port'), app.get('ip'), function(){
   console.log("Express server listening on port " + app.get('port'));
 });
